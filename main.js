37e3049091518061531fef9857a3c008b4ebd7fa
tsoury@gmail.com <tsoury@gmail.com>
2023-11-18 11:46:10 +0700

    HTML done, JS error

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
-import Typed from 'typed.js';
+import Typed from "typed.js";
+import confetti from "canvas-confetti";
 
 const typed = new Typed('#element', {
   strings: ['Developer', 'Full Stack', 'Engineer'],
   typeSpeed: 50,
+});
+
+const appreciateBtn = document.getElementById("appreciate");
+appreciateBtn.addEventListener("click", () => {
+  const myCanvas = document.createElement("canvas");
+  myCanvas.style.position = "fixed";
+  myCanvas.style.inset = 0;
+  myCanvas.style.width = "100vw";
+  myCanvas.style.height = "100vh";
+  myCanvas.style.zIndex = -1;
+  document.body.appendChild(myCanvas);
+
+  const myConfetti = confetti.create(myCanvas, {
+    resize: true,
+    useWorker: true,
+  });
+  myConfetti({
+    particleCount: 100,
+    spread: 160,
+  });
 });
